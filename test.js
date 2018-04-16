c47d885129d82fdb9c9f9c59184b9230f103c332
谢雨 <xieyu@xieyudeMacBook-Pro.local>
2018-04-17 00:32:18 +0800

    添加eslint 配置文件

--- test.js
+++ test.js
@@ -2,20 +2,20 @@
 /*兼容ie,ff获取元素的style*/
 //getStyle(div1,background);获取div1的背景样式
 function getStyle(obj,name){
-			if(obj.currentStyle)
-			{
-				return obj.currentStyle[name];
-			}
-			else{
-				return getComputedStyle(obj,false)[name];
-			}
-		}
+	if(obj.currentStyle)
+	{
+		return obj.currentStyle[name];
+	}
+	else{
+		return getComputedStyle(obj,false)[name];
+	}
+}
 
 /*通过className获取元素*/
 //getByCalss(div1,box);获取div1下class =box的元素
 function getByClass(oParent,sClass){
 	var result = [];
-	var aEle=oParent.getElementsByTagName('*');
+	var aEle=oParent.getElementsByTagName("*");
 	for(var i = 0;i<aEle.length;i++)
 	{
 		if(aEle[i].className==sClass)
@@ -26,3 +26,5 @@ function getByClass(oParent,sClass){
 	}
 	return result;
 }
+
+export { getStyle, getByClass} ;
